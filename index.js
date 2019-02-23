adf4a55e4a5ad91c5a8d668d623cc08c428f8ce2
mikihonig <vegyigyumi2@gmail.com>
2019-02-23 14:16:07 +0100

    aaa

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-const buttonElement1 = document.getElementById('btn1');
-let ujtodo=1;
+// const buttonElement1 = document.getElementById('btn1');
+// let ujtodo=1;
 
 
-buttonElement1.addEventListener('click', function (event) {
-  ujtodo=document.getElementById("name").value;
-  console.log (event);
-  console.log (document.getElementById("name").value);
-  if (document.getElementById('name').value === "") {}
-  else {
-    $('ul').append(`<li> ${ujtodo} </li>`);
-    document.getElementById('name').value = '';}
+fb = firebase.database();
+
+// adatok importalasa az adatbazisbol
+
+fb.ref("todo").once('value').then(data => {
+  data.forEach(element => {
+    $('ul').append(`<li> ${element.child('name').val()} ${element.child('darab').val()}</li>`);
+  })
 });
+
+// gombnyomas
+
+$('#kattincsButton').on('click', (event) => {
+  event.preventDefault();
+  let ujtodo = $('#name').val();
+  let ujdarab = $('#darab').val();
+  let dataToSave = {name: ujtodo, darab: ujdarab};
+  
+  if (ujtodo === "") {}
+  else {fb.ref("todo").push(dataToSave)};
+  
+  if (ujtodo === "") {}
+  else {$('ul').append(`<li> ${ujtodo} ${ujdarab}</li>`)};
+
+  document.getElementById('name').value = '';
+  document.getElementById('darab').value = '';
+
+});
+
+// buttonElement1.addEventListener('click', function (event) {
+//   ujtodo=document.getElementById("name").value;
+//   ujdarab=document.getElementById("darab").value;
+//   console.log (event);
+//   console.log (document.getElementById("name").value);
+
+  // kiiratas
+  // if (document.getElementById('name').value === "") {}
+  // else {
+  //   $('ul').append(`<li> ${ujtodo} ${ujdarab}</li>`);
+
+  // adatbazisba rogzites
+  // let dataToSave = {
+  //   name: ujtodo,
+  //   darab: ujdarab
+  // };
+  // fb.ref("todo").push(dataToSave);
+
+//   // mezok nullazasa
+//   document.getElementById('name').value = '';}
+// });
 
 $('ul').on('click', (event) => {
   $(event.target).css('text-decoration', 'line-through')
+});
+
+$('#torles').on('click', (torles) => {
+  fb.ref("todo").remove();
+  location.reload(true);
 });
